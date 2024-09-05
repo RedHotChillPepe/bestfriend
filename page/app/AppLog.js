6c78f1dffc76d3eff785bd8c8b051d1993145a9d
Alina <changatasan@gmail.com>
2024-09-05 17:07:53 +0300

    react-native-fs, react-native-document-picker added button and modal for future folders added changed more styling

--- page/app/AppLog.js
+++ page/app/AppLog.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-native-paper';
+import { Modal, Provider } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import ProfileScreen from './ProfileScreen';
@@ -10,9 +10,11 @@ import FairyTales from './Sound/fairyTales';
 import MyRecordingScreen from './MyRecordingScreen';
 import Help from './Sound/Help';
 import Riddles from './Sound/Riddles';
+import { Pressable, StyleSheet, Text, View, VirtualizedList } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
 
 function ReadySoundsStack() {
   return (
@@ -26,10 +28,14 @@ function ReadySoundsStack() {
 }
 
 export default function AppLog() {
+  
+  const [modalVisible, setModalVisible] = useState(false);
+
+
   return (
     <Provider>
       <Tab.Navigator
-        initialRouteName="Библиотека звуков"
+        initialRouteName="Библиотека звуков" 
         screenOptions={{
           tabBarActiveTintColor: '#6f9c3d',
           tabBarInactiveTintColor: '#5c5c5c',
@@ -40,12 +46,19 @@ export default function AppLog() {
           headerTitleStyle: {
             fontFamily: 'Comfortaa_600SemiBold',
             fontSize: 18,
-            color: '#5c5c5c'
+            color: '#FFF'
           },
           headerStyle:{
               borderBottomWidth:1,
               borderBottomColor:'#d9d9d9'
-          }
+          },
+          headerRight:()=>(
+            <Pressable onPress={()=>setModalVisible(true)}>
+              <MaterialCommunityIcons name='plus-circle-outline' color="#FFF" size={45} style={{paddingRight:25}}/>
+            </Pressable>
+          ),
+
+          
         }}
       >
         <Tab.Screen
@@ -53,6 +66,11 @@ export default function AppLog() {
           component={ReadySoundsStack}
           options={{
             tabBarLabel: 'Библиотека',
+            headerStyle:{
+              borderBottomLeftRadius:48,
+              borderBottomRightRadius:48,
+              backgroundColor:"#3C62DD"
+            },
             tabBarIcon: ({ color }) => (
               <Ionicons name="library" size={30} color={color} />
             ),
@@ -62,6 +80,9 @@ export default function AppLog() {
           name="Свои записи"
           component={MyRecordingScreen}
           options={{
+            headerStyle:{
+              backgroundColor:"#3C62DD"
+            },
             tabBarLabel: 'Свои записи',
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="microphone" color={color} size={30} />
@@ -73,12 +94,57 @@ export default function AppLog() {
           component={ProfileScreen}
           options={{
             tabBarLabel: 'Профиль',
+            headerStyle:{
+              backgroundColor:"#3C62DD"
+            },
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="account" color={color} size={30} />
             ),
           }}
         />
       </Tab.Navigator>
+
+      {
+        modalVisible ?
+
+        <Modal transparent={true} animationType={'none'} visible={modalVisible}  onRequestClose={()=>{setModalVisible(false)}} onDismiss={()=>{setModalVisible(false)}}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text>RRRR</Text>
+            </View>
+          </View>
+        </Modal>
+        : <View></View>
+      }
+        
+
+      
+
+
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+},
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  }
+})
